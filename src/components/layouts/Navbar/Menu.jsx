b2ee5c299663c62362d7d909d53312e270c22f44
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-05 20:37:36 +0500

    Fixes eslint issues

--- src/components/layouts/Navbar/Menu.jsx
+++ src/components/layouts/Navbar/Menu.jsx
@@ -4,23 +4,23 @@ import { Link } from 'react-router-dom';
 function Menu() {
   return (
     <nav className="navbar">
-        <ul className="navbar-nav">
-            <li className="nav-item">
-                <Link to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-                <Link to="/books">Books</Link>
-            </li>
-            <li className="nav-item">
-                <Link to="/about">About Malcolm</Link>
-            </li>
-            <li className="nav-item">
-                <Link to="/events">Events</Link>
-            </li>
-            <li className="nav-item">
-                <Link to="/podcasts">Podcasts</Link>
-            </li>
-        </ul>
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/books">Books</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/about">About Malcolm</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/events">Events</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/podcasts">Podcasts</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
