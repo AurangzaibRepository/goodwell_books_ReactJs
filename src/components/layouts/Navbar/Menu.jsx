38150c063a283c9f095808570c436766a4f7a704
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-05 19:20:34 +0500

    Imports Link

--- src/components/layouts/Navbar/Menu.jsx
+++ src/components/layouts/Navbar/Menu.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Menu() {
-  return ();
+  return (
+    <nav className="navbar">
+        <ul className="navbar-nav">
+            <li className="nav-item">
+
+            </li>
+            <li className="nav-item">asd</li>
+            <li className="nav-item">asd</li>
+            <li className="nav-item">asd</li>
+        </ul>
+    </nav>
+  );
 }
 
 export default Menu;
