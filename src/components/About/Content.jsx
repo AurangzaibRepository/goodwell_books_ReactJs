478ff1854b0b286c863af14d0f407811401de179
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-07 01:57:18 +0500

    Imports Link component

--- src/components/About/Content.jsx
+++ src/components/About/Content.jsx
@@ -1,13 +1,36 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function Content({ title, description }) {
-  return();
+  return (
+    <>
+      <label className="title mb-5">{title}</label>
+      <div id="dv-contents">
+        <div className="row">
+          <div className="col-md-6 text-center pe-0">
+            <img src={picture} alt="author" />
+          </div>
+          <div className="col-md-6 ps-0">
+            <p>{description}</p>
+            <label className="sub-title mt-4 mb-2">Events</label>
+            <p>
+              {'Malcolm Gladwell is going on tour. '}
+              <Link to="/events">Click here</Link>
+              {' to find out where he’ll be.'}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 Content.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-}
+};
 
 export default Content;
