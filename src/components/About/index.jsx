20c16626528c45ca110792d949b7b4fac291ac49
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-06 19:42:58 +0500

    Implements get data

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import AboutService from '../../services/AboutService';
 
 function About() {
   const [data, setData] = useState();
 
+  const getData = async () => {
+    const aboutData = await AboutService.getData();
+    setData(aboutData);
+  };
+
   useEffect(() => {
     document.title = `${process.env.REACT_APP_APP_NAME} - About`;
+    getData();
   });
 
   return (
