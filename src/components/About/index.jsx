205b81679566487778bad8c90501371202c5f506
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-07 02:00:58 +0500

    Imports content component

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,8 +1,7 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import AboutService from '../../services/AboutService';
-import picture from '../../assets/about/sloman.webp';
+import Content from './Content';
 import './style.css';
 
 function About() {
@@ -22,25 +21,7 @@ function About() {
     <div id="dv-about">
       {data
       && (
-        <>
-          <label className="title mb-5">{data.title}</label>
-          <div id="dv-contents">
-            <div className="row">
-              <div className="col-md-6 text-center pe-0">
-                <img src={picture} alt="author" />
-              </div>
-              <div className="col-md-6 ps-0">
-                <p>{data.description}</p>
-                <label className="sub-title mt-4 mb-2">Events</label>
-                <p>
-                  {'Malcolm Gladwell is going on tour. '}
-                  <Link to="/events">Click here</Link>
-                  {' to find out where he’ll be.'}
-                </p>
-              </div>
-            </div>
-          </div>
-        </>
+         
       )}
     </div>
   );
