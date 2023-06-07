6428beaf54da29fb59ff064b6ae30fcc77a5ffb8
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-07 19:11:29 +0500

    Imports font awesome icons

--- src/components/About/SocialMediaIcons.jsx
+++ src/components/About/SocialMediaIcons.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faTwitter,
+    faInstagram,
+    faFacebook,
+} from '@fortawesome/free-brands-svg-icons';
 
 function SocialMediaIcons() {
   return ();
