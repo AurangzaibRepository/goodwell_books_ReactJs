25b02c9f4bddd75f1345ef0ad2d0c0eb3c622e1b
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-07 19:29:22 +0500

    Renders icon

--- src/components/About/SocialMediaIcons.jsx
+++ src/components/About/SocialMediaIcons.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-    faTwitter,
-    faInstagram,
-    faFacebook,
+  faTwitter,
+  faInstagram,
+  faFacebook,
 } from '@fortawesome/free-brands-svg-icons';
 
 function SocialMediaIcons() {
-  return ();
+  return (
+    <>
+      <FontAwesomeIcon icon={faTwitter} />
+      <FontAwesomeIcon icon={faInstagram} />
+      <FontAwesomeIcon icon={faFacebook} />
+    </>
+  );
 }
 
 export default SocialMediaIcons;
