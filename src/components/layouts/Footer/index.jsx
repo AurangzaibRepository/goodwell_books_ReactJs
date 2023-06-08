9656ee95a4f7d86a45ff789b735d6453b9af6520
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-08 21:04:52 +0500

    Adds contents

--- src/components/layouts/Footer/index.jsx
+++ src/components/layouts/Footer/index.jsx
@@ -6,6 +6,21 @@ function Footer() {
   return (
     <div id="dv-footer" className="text-center">
       <img src={footerLogo} alt="logo" />
+      <p>
+        {'© 2023 Hachette Book Group | '}
+        <a href="https://www.hachettebookgroup.com/terms-and-policies/terms-of-use/?_ga=2.223350302.408625558.1686233794-1848420058.1686233788">
+          {'Terms of Use | '}
+        </a>
+        <a href="https://www.hachettebookgroup.com/terms-and-policies/privacy-policy/">
+          {'Privacy Policy | '}
+        </a>
+        <a href="https://www.hachettebookgroup.biz/contact-us/">
+          {'Contact Us | '}
+        </a>
+        <a href="https://hbgusa.zendesk.com/hc/en-us/articles/360047685472-HBG-CCPA-Privacy-Rights-Tool">
+          Do Not Steal My Personal Information
+        </a>
+      </p>
     </div>
   );
 }
