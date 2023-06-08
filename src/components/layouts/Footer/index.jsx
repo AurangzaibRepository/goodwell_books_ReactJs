12139ecb475f2aa15107fd24c84edc10ca39f5cf
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-08 22:29:25 +0500

    Renders items

--- src/components/layouts/Footer/index.jsx
+++ src/components/layouts/Footer/index.jsx
@@ -8,7 +8,7 @@ function Footer() {
 
   const getData = async () => {
     const footerData = await FooterService.getData();
-    setData(footerData);
+    setData(footerData.links);
   };
 
   useEffect(() => {
@@ -20,18 +20,14 @@ function Footer() {
       <img src={footerLogo} alt="logo" />
       <p>
         {'© 2023 Hachette Book Group | '}
-        <a href="https://www.hachettebookgroup.com/terms-and-policies/terms-of-use/?_ga=2.223350302.408625558.1686233794-1848420058.1686233788">
-          {'Terms of Use | '}
-        </a>
-        <a href="https://www.hachettebookgroup.com/terms-and-policies/privacy-policy/">
-          {'Privacy Policy | '}
-        </a>
-        <a href="https://www.hachettebookgroup.biz/contact-us/">
-          {'Contact Us | '}
-        </a>
-        <a href="https://hbgusa.zendesk.com/hc/en-us/articles/360047685472-HBG-CCPA-Privacy-Rights-Tool">
-          Do Not Steal My Personal Information
-        </a>
+        {data
+        && (
+          data.map((item) => (
+            <a href={item.url} key={item.id}>
+              {item.title}
+            </a>
+          ))
+        )}
       </p>
     </div>
   );
