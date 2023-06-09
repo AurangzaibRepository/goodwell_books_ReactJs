df059143dbc341dab1d7b3f27ea97d4ebe4f7eef
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-09 21:07:50 +0500

    Imports images

--- src/components/Podcasts/List.jsx
+++ src/components/Podcasts/List.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import image1 from '../../assets/podcasts/podcast1.webp';
+import image2 from '../../assets/podcasts/podcast2.webp';
+import image3 from '../../assets/podcasts/podcast3.webp';
+import image4 from '../../assets/podcasts/podcast4.webp';
+import image5 from '../../assets/podcasts/podcast5.webp';
+import image6 from '../../assets/podcasts/podcast6.webp';
 
 function List() {
   return ();
