cb56d713c85b485999add1723f716ca45846e79a
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-09 23:38:13 +0500

    Removes prop

--- src/components/Podcasts/List.jsx
+++ src/components/Podcasts/List.jsx
@@ -1,7 +1,5 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-import PropTypes from 'prop-types';
 import image1 from '../../assets/podcasts/podcast1.webp';
 import image2 from '../../assets/podcasts/podcast2.webp';
 import image3 from '../../assets/podcasts/podcast3.webp';
@@ -9,7 +7,7 @@ import image4 from '../../assets/podcasts/podcast4.webp';
 import image5 from '../../assets/podcasts/podcast5.webp';
 import image6 from '../../assets/podcasts/podcast6.webp';
 
-function List({ list }) {
+function List() {
   const imageList = [
     image1,
     image2,
@@ -41,9 +39,5 @@ function List({ list }) {
     </>
   );
 }
-
-List.propTypes = {
-  list: PropTypes.instanceOf(Array).isRequired,
-};
 
 export default List;
