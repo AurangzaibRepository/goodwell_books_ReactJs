98a4fc81f06d668297040d30776991fec378a27f
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-10 00:33:39 +0500

    Implements function to retrieve data in service

--- src/services/BooksService.jsx
+++ src/services/BooksService.jsx
@@ -1,5 +1,10 @@
 const BooksService = {
+  getData: async () => {
+    let data = await fetch('data/books_data.json');
+    data = await data.json();
 
+    return data.books;
+  },
 };
 
 export default BooksService;
