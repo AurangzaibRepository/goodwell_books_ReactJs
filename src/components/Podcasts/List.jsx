43a93f90e5b041dec29cba98dca7ef4f3b5fe8ae
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-09 21:12:07 +0500

    Implements prop types validation

--- src/components/Podcasts/List.jsx
+++ src/components/Podcasts/List.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import image1 from '../../assets/podcasts/podcast1.webp';
 import image2 from '../../assets/podcasts/podcast2.webp';
 import image3 from '../../assets/podcasts/podcast3.webp';
@@ -6,8 +7,12 @@ import image4 from '../../assets/podcasts/podcast4.webp';
 import image5 from '../../assets/podcasts/podcast5.webp';
 import image6 from '../../assets/podcasts/podcast6.webp';
 
-function List() {
+function List({ list }) {
   return ();
 }
+
+List.propTypes = {
+  list: PropTypes.instanceOf(Array).isRequired,
+};
 
 export default List;
