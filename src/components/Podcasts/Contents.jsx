ec39216c0cf2abbe2850600a9171a236ebfb6d23
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-09 20:47:44 +0500

    Imports images

--- src/components/Podcasts/Contents.jsx
+++ src/components/Podcasts/Contents.jsx
@@ -3,6 +3,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactHtmlParser from 'react-html-parser';
+import image1 from '../../assets/podcasts/podcast1.webp';
+import image2 from '../../assets/podcasts/podcast2.webp';
+import image3 from '../../assets/podcasts/podcast3.webp';
+import image4 from '../../assets/podcasts/podcast4.webp';
+import image5 from '../../assets/podcasts/podcast5.webp';
+import image6 from '../../assets/podcasts/podcast6.webp';
 
 function Contents({
   title,
