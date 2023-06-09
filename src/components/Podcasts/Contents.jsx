afde1107535c1ddb6a5239600302237a0700bf15
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-09 23:11:55 +0500

    Removes list prop

--- src/components/Podcasts/Contents.jsx
+++ src/components/Podcasts/Contents.jsx
@@ -8,13 +8,12 @@ import List from './List';
 function Contents({
   title,
   description,
-  list,
 }) {
   return (
     <>
       <label className="title mb-5">{title}</label>
       <p>{ReactHtmlParser(description)}</p>
-      <List list={list} />
+      <List />
     </>
   );
 }
@@ -22,7 +21,6 @@ function Contents({
 Contents.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  list: PropTypes.instanceOf(Array).isRequired,
 };
 
 export default Contents;
