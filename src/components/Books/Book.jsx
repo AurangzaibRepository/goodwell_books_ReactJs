aaabfa021a71bffc138c7ee24c554e38eac21e58
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-12 18:56:07 +0500

    Imports images

--- src/components/Books/Book.jsx
+++ src/components/Books/Book.jsx
@@ -1,12 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
+import BookImage1 from '../../assets/books/book1.webp';
+import BookImage2 from '../../assets/books/book2.webp';
+import BookImage3 from '../../assets/books/book3.webp';
+import BookImage4 from '../../assets/books/book4.webp';
+import BookImage5 from '../../assets/books/book5.webp';
 
 function Book({
   title,
   description,
   buyLink,
 }) {
- return ();
+ return (
+  <div className="row mt-5">
+    <div className="col-md-4">
+      
+    </div>      
+  </div>
+ );
 }
 
 Book.propTypes = {
