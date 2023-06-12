b96f3128d39c015e9ab3f8cee5f720248e4d440f
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-12 19:12:09 +0500

    Imports book component

--- src/components/Books/Contents.jsx
+++ src/components/Books/Contents.jsx
@@ -2,6 +2,7 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import PropTypes from 'prop-types';
+import Book from './Book';
 
 function Contents({ title, books }) {
   return (
