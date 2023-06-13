168a6db3a29a2c3588867a8cb4b9cc4df9b48851
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-13 19:21:27 +0500

    Implements service function

--- src/services/BookInfoService.jsx
+++ src/services/BookInfoService.jsx
@@ -1,5 +1,10 @@
 const BookInfoService = {
+  getData: async () => {
+    let data = await fetch('data/bookinfo_data.json');
+    data = await data.json();
 
+    return data;
+  },
 };
 
 export default BookInfoService;
