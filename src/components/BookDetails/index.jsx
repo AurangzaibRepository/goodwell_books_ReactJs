ee0cdab70be287902f053d94d025f4b83df41fac
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-13 21:25:49 +0500

    Imports use params hook

--- src/components/BookDetails/index.jsx
+++ src/components/BookDetails/index.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import BookInfoService from '../../services/BookInfoService';
 
 function BookDetails() {
