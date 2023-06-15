d81f819dd4e8e69f1afa412b0de02a885b47f61b
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-15 22:36:36 +0500

    Adds div

--- src/components/BookDetails/CoverLinks.jsx
+++ src/components/BookDetails/CoverLinks.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import ExternalLink from '../layouts/ExternalLink';
 
 function CoverLinks() {
-  return ();
+  return (
+    <div id="dv-cover-links" className="pt-4 pb-4">
+    </div>
+  );
 }
 
 export default CoverLinks;
