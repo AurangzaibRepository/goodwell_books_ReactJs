4de869c54b2b91b5e7a59f82892a32f90ca1c8ed
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-15 23:19:20 +0500

    Specifies margin top class

--- src/components/BookDetails/BuyLinks.jsx
+++ src/components/BookDetails/BuyLinks.jsx
@@ -1,8 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import PropTypes from 'prop-types';
 
 function BuyLinks({ buyLinks }) {
-  return ();
+  return (
+    <div id="dv-buy-links" className="mt-4">
+      <label>Buy Now:</label>
+    </div>
+  );
 }
 
 BuyLinks.propTypes = {
