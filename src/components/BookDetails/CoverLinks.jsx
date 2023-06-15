cf44fe02f3ea5fd6e60eaf739b5bf61431c2e23c
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-15 22:39:48 +0500

    Adds external link component

--- src/components/BookDetails/CoverLinks.jsx
+++ src/components/BookDetails/CoverLinks.jsx
@@ -4,6 +4,18 @@ import ExternalLink from '../layouts/ExternalLink';
 function CoverLinks() {
   return (
     <div id="dv-cover-links" className="pt-4 pb-4">
+      <ExternalLink
+        title="HARDCOVER"
+        url="#"
+      />
+      <ExternalLink
+        title="EBOOK"
+        url="#"
+      />
+      <ExternalLink
+        title="AURDIOBOOK"
+        url="#"
+      />
     </div>
   );
 }
