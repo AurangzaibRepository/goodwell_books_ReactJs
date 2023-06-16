834da3d53054a0f833e3a61f59fe23690a5e710f
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-16 19:18:51 +0500

    Renders buy links

--- src/components/BookDetails/BuyLinks.jsx
+++ src/components/BookDetails/BuyLinks.jsx
@@ -2,11 +2,19 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import PropTypes from 'prop-types';
+import ExternalLink from '../layouts/ExternalLink';
 
 function BuyLinks({ buyLinks }) {
   return (
     <div id="dv-buy-links" className="mt-4">
       <label>Buy Now:</label>
+      {buyLinks.map((item) => (
+        <ExternalLink
+          key={item.id}
+          title={item.title}
+          url={item.link}
+        />
+      ))}
     </div>
   );
 }
