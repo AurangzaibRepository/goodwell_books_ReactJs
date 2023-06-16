626fce6b6e8e02c4ce1118b1dccac0b6fb41e6e8
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-16 21:24:43 +0500

    Imports Image component

--- src/components/About/Content.jsx
+++ src/components/About/Content.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import Image from '../layouts/Image';
 import SocialMediaIcons from './SocialMediaIcons';
 import picture from '../../assets/about/sloman.webp';
 
