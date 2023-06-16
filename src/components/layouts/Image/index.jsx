fcf8648cc6f7de7d49122b5fda58b44a4e7900a2
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-16 20:40:47 +0500

    Implements prop types validation

--- src/components/layouts/Image/index.jsx
+++ src/components/layouts/Image/index.jsx
@@ -4,10 +4,17 @@ import PropTypes from 'prop-types';
 function Image({
   src,
   alt,
-  classSpecifier,
+  classIdentifier,
   id,
 }) {
   return ();
 }
+
+Image.propTypes = {
+  src: PropTypes.node.isRequired,
+  alt: PropTypes.string.isRequired,
+  classIdentifier: PropTypes.string,
+  id: PropTypes.string,
+};
 
 export default Image;
