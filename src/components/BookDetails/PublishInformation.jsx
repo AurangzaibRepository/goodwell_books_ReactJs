ada87e2cd620864fa9753c5def5f53400ab06340
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-17 00:46:53 +0500

    Implements prop types validation

--- src/components/BookDetails/PublishInformation.jsx
+++ src/components/BookDetails/PublishInformation.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function PublishInformation({ publishInfo, requestLink }) {
+function PublishInformation({ publishData, requestLink }) {
   return ();
 }
+
+PublishInformation.propTypes = {
+  publishData: PropTypes.object.isRequired,
+  requestLink: PropTypes.string.isRequired,
+};
 
 export default PublishInformation;
