3b5d0c6932c3933dd0910a91f7303357d745d229
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-16 22:09:54 +0500

    Adds label

--- src/components/layouts/Label/index.jsx
+++ src/components/layouts/Label/index.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -6,7 +8,14 @@ function Label({
   id,
   classIdentifier,
 }) {
-  return ();
+  return (
+    <label
+      id={id}
+      className={classIdentifier}
+    >
+      {value}
+    </label>
+  );
 }
 
 Label.propTypes = {
