bff7841247f02f9eb117196dad17b913bc3c10e5
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-19 18:38:27 +0500

    Imports font awesome icon

--- src/components/BookDetails/PublishIcon.jsx
+++ src/components/BookDetails/PublishIcon.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function PublishIcon({
   title,
