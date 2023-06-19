04a2eeb262168172968428019cbaa1bc80fea130
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-19 21:48:09 +0500

    Adds body

--- src/components/BookDetails/Description.jsx
+++ src/components/BookDetails/Description.jsx
@@ -1,9 +1,20 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
 import Label from '../layouts/Label';
 
 function Description({ description }) {
-  return ();
+  return (
+    <>
+      <Label
+        id="label-description"
+        value="Description"
+      />
+      <p>
+        {description}
+      </p>
+    </>
+  );
 }
 
 Description.propTypes = {
