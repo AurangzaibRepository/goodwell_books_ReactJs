e0ba3dc1a8452c3cfda3e7142497a5c348f1f228
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-20 17:50:24 +0500

    Adds prop

--- src/components/BookDetails/Excerpt.jsx
+++ src/components/BookDetails/Excerpt.jsx
@@ -1,8 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
 import Label from '../layouts/Label';
 
-function Excerpt() {
+function Excerpt({ link }) {
   return (
     <Label
       value="LISTEN TO AN EXCERPT"
