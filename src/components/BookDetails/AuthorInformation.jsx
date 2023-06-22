d0b6ca1f4b159fb82fb1fb0ff1f0d39e02a26c06
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-22 19:58:47 +0500

    Renders Image component

--- src/components/BookDetails/AuthorInformation.jsx
+++ src/components/BookDetails/AuthorInformation.jsx
@@ -7,7 +7,12 @@ import picture from '../../assets/about/sloman.webp';
 function AuthorInformation({ authorInformation }) {
   return (
     <div className="row" id="dv-author-information">
-      <div className="col-md-6"></div>
+      <div className="col-md-6">
+        <Image
+          src={picture}
+          alt="author"
+        />
+      </div>
       <div className="col-md-6"></div>
     </div>
   );
