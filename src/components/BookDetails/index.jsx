acdc395d9e347a5ef3d48e11f1e9981cc275712b
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-22 23:45:40 +0500

    Imports Related article component

--- src/components/BookDetails/index.jsx
+++ src/components/BookDetails/index.jsx
@@ -8,6 +8,7 @@ import Excerpt from './Excerpt';
 import Praises from './Praises';
 import IFrame from '../layouts/IFrame';
 import AuthorInformation from './AuthorInformation';
+import RelatedArticles from './RelatedArticles';
 import BookInfoService from '../../services/BookInfoService';
 import './style.css';
 
