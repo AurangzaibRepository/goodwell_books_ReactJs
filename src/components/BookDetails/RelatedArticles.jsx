49fd4f9c69622d0d085c2e33649f04b32fc489e1
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-22 23:34:38 +0500

    Imports carouel component

--- src/components/BookDetails/RelatedArticles.jsx
+++ src/components/BookDetails/RelatedArticles.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
 import Image1 from '../../assets/books/related_article1.webp';
 import Image2 from '../../assets/books/related_article2.webp';
 import Image3 from '../../assets/books/related_article3.webp';
