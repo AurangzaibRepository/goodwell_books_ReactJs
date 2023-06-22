66d46086f295e94f5bf87a94c4485d82e0e6c9e3
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-22 23:38:20 +0500

    Creates image array

--- src/components/BookDetails/RelatedArticles.jsx
+++ src/components/BookDetails/RelatedArticles.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import Image1 from '../../assets/books/related_article1.webp';
@@ -6,7 +7,18 @@ import Image3 from '../../assets/books/related_article3.webp';
 import Image4 from '../../assets/books/related_article4.webp';
 
 function RelatedArticles() {
-  return ();
+  const imageList = [
+    Image1,
+    Image2,
+    Image3,
+    Image4,
+  ];
+
+  return (
+    <Carousel>
+
+    </Carousel>
+  );
 }
 
 export default RelatedArticles;
