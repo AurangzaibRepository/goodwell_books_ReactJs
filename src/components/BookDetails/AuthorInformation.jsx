11edf2b590ec8a69d4dce38c58331f427c44f286
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-22 20:00:05 +0500

    Imports Label component

--- src/components/BookDetails/AuthorInformation.jsx
+++ src/components/BookDetails/AuthorInformation.jsx
@@ -2,18 +2,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Image from '../layouts/Image';
+import Label from '../layouts/Label';
 import picture from '../../assets/about/sloman.webp';
 
 function AuthorInformation({ authorInformation }) {
   return (
     <div className="row" id="dv-author-information">
-      <div className="col-md-6">
+      <div className="col-md-4">
         <Image
           src={picture}
           alt="author"
         />
       </div>
-      <div className="col-md-6"></div>
+      <div className="col-md-8">
+
+      </div>
     </div>
   );
 }
