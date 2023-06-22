5c0113fb2a8abf692b211d9ee65e4b6b271f3af2
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-22 23:33:54 +0500

    Imports images

--- src/components/BookDetails/RelatedArticles.jsx
+++ src/components/BookDetails/RelatedArticles.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import Image1 from '../../assets/books/related_article1.webp';
+import Image2 from '../../assets/books/related_article2.webp';
+import Image3 from '../../assets/books/related_article3.webp';
+import Image4 from '../../assets/books/related_article4.webp';
 
 function RelatedArticles() {
   return ();
