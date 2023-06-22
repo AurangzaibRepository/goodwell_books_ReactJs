4d00adcb537791145d6bae8dfc0f76cc8cb6c2f1
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-22 19:57:11 +0500

    Imports Label component

--- src/components/BookDetails/AuthorInformation.jsx
+++ src/components/BookDetails/AuthorInformation.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
+import Image from '../layouts/Image';
 
 function AuthorInformation({ authorInformation }) {
   return (
