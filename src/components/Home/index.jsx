bcb02aa79168733b3c949e0cf404647c5bf11900
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-23 18:25:41 +0500

    Implements function to retrieve data from service

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,9 +4,15 @@ import HomeService from '../../services/HomeService';
 function Home() {
   const [data, setData] = useState();
 
+  const getData = async () => {
+    const homeData = await HomeService.getData();
+    setData(homeData);
+  };
+
   useEffect(() => {
     document.title = `${process.env.REACT_APP_APP_NAME} - Home`;
-  });
+    getData();
+  }, []);
 
   return ();
 }
