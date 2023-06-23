96e52bd8277f061248a6cae43735322e9e028853
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-24 00:51:25 +0500

    Imports Image component

--- src/components/Home/RecommendedBook.jsx
+++ src/components/Home/RecommendedBook.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
+import Image from '../layouts/Image';
 import BookCover from '../../assets/books/book1.webp';
 
 function RecommendedBook({
