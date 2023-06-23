30495a42903e5551225f00ace8cc481818757de7
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-24 01:05:35 +0500

    Imports Upcoming events

---
+++ src/components/Home/RecommendedBook/index.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import PropTypes from 'prop-types';
+import UpcomingEvents from './UpcomingEvents';
+
+function RecommendedBook({
+  title,
+  subTitle,
+  caption,
+}) {
+  return (
+    <div className="row dv-section" id="dv-recommended-book">
+      <div className="col-md-6">
+      </div>
+      <div className="col-md-6">
+      </div>
+    </div>
+  );
+}
+
+RecommendedBook.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string.isRequired,
+  caption: PropTypes.string.isRequired,
+};
+
+export default RecommendedBook;
