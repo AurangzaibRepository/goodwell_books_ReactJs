bb3fd87001dd6458c55d64fbc7704feef3e5946e
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-23 20:20:23 +0500

    Adds div and imports label component

--- src/components/Home/Hardcover.jsx
+++ src/components/Home/Hardcover.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import Label from '../layouts/Label';
 
 function Hardcover() {
-  return ();
+  return (
+    <div className="row mt-4">
+      <div className="col-md-6">
+
+      </div>
+      <div className="col-md-6">
+
+      </div>
+    </div>
+  );
 }
 
 export default Hardcover;
